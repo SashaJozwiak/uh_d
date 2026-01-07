325e44d7b959ae42d510c1890183ce88eba56e23
SashaJozwiak <wmdz88@gmail.com>
2026-01-07 18:10:14 +0500

    fix: aug6

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,3 +1,10 @@
+
+import { useAuth } from "react-oidc-context";
+
 export default function Main() {
+
+    const auth = useAuth();
+    console.log('auth data:', auth);
+
     return <h1>Dashboard — You are logged in!</h1>;
 }
